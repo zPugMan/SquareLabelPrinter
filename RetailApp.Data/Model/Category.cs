4ad4554b39fe5ad06cbc928e6e2450ccbe4917d1
Zachary Bethem <zbethem@outlook.com>
2019-05-26 18:08:38 -0700

    commit point: category and product retrieval from square completed

---
+++ RetailApp.Data/Model/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetailApp.Data.Model
+{
+    public class Category
+    {
+        public string SquareId { get; set; }
+        public string SquareName { get; set; }
+    }
+}
