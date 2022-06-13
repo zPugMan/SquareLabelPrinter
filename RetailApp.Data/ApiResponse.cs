9f074e1c7b7cc2428d9d3d7cffd4d29a9411fe34
Zachary Bethem <zbethem@outlook.com>
2022-06-12 18:30:18 -0700

    completed inventory service to support updates

---
+++ RetailApp.Data/ApiResponse.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Square.Connect.Model;
+
+namespace RetailApp.Data
+{
+    public class ApiResponse
+    {
+        public List<ApiError> Errors { get; set; }
+        public bool IsSuccess { get; set; }
+
+        public ApiResponse() { }
+
+        public ApiResponse(List<ApiError> errors, bool success)
+        {
+            Errors = errors;
+            IsSuccess = success;
+        }
+
+        public ApiResponse(List<Error> errors, bool success)
+        {
+            IsSuccess = success;
+
+            if(errors != null && errors.Count>0)
+                Errors = errors.ConvertAll(new Converter<Error, ApiError>(ApiError.SquareErrorConverter));
+        }
+
+        public ApiResponse(string errorMessage, bool success)
+        {
+            IsSuccess = success;
+            Errors = new List<ApiError>() { new ApiError(errorMessage) };
+        }
+    }
+}
--- RetailApp.Data/InventoryService.cs
+++ RetailApp.Data/InventoryService.cs
@@ -4,6 +4,7 @@ using Square.Connect.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,36 +12,93 @@ namespace RetailApp.Data
 {
     public class InventoryService
     {
-        private InventoryApi inventoryAPI;
+        private IInventoryApi inventoryAPI;
+        internal const string SANDBOX = "EAAAEOuEbej1xTRYe87QtBrxFosUNbeyF3Cn15PcNFFCltL-6_LbT-0mr2AL7zha";
+        internal const string PROD = "***REMOVED***";
+        internal const string LOCATION = "LEASAZNP0VFC5";   //SANDBOX location
 
         public InventoryService()
         {
-            Configuration.Default.AccessToken = "***REMOVED***";
-            inventoryAPI = new InventoryApi();
+            //Configuration.Default.AccessToken = "EAAAEOuEbej1xTRYe87QtBrxFosUNbeyF3Cn15PcNFFCltL-6_LbT-0mr2AL7zha";  //TODO refactor this "***REMOVED***";
+            //Configuration.Default.AccessToken = SANDBOX;
+            var config = new Configuration(new ApiClient(basePath: "https://connect.squareupsandbox.com"));
+            config.AccessToken = SANDBOX;
+            inventoryAPI = new InventoryApi(config);
         }
 
-        public InventoryCount GetInventory(String id)
+        public InventoryService(IInventoryApi api)
         {
-            var response = inventoryAPI.RetrieveInventoryCount(id);
-            return response.Counts.First();
+            inventoryAPI = api;
+            //inventoryAPI.Configuration.AccessToken = SANDBOX;
         }
 
-        public async Task<InventoryCount> GetInventoryAsync(String id)
+        public InventoryCount GetInventory(String catalogObjectId)
         {
-            var response = await inventoryAPI.RetrieveInventoryCountAsync(id);
-            return response.Counts.First();
+            try
+            {
+                var response = inventoryAPI.RetrieveInventoryCountWithHttpInfo(catalogObjectId, null, null);
+                if (response.Data.Counts == null)
+                    return null;
+
+                return response.Data.Counts.First();
+            } catch(ApiException e)
+            {
+                return null;
+            } catch(Exception e)
+            {
+                return null;
+            }
+            
         }
 
-        //TODO inventory update
-        public void AddInventory(Guid guid, InventoryChange change)
+        //public async Task<InventoryCount> GetInventoryAsync(String id)
+        //{
+        //    var response = await inventoryAPI.RetrieveInventoryCountAsync(id);
+        //    return response.Counts.First();
+        //}
+
+        public ApiResponse AddInventory(string catalogObjectId, int changeQuantity)
         {
-            List<InventoryChange> changes = new List<InventoryChange>();
-            changes.Add(change);
+            if (changeQuantity < 1)
+                return new ApiResponse("Invalid amount to add inventory", false);
+
+            var itemAdjustment = new InventoryAdjustment(
+                null
+                , null
+                , InventoryAdjustment.FromStateEnum.NONE
+                , InventoryAdjustment.ToStateEnum.INSTOCK
+                , LocationId: LOCATION
+                , CatalogObjectId: catalogObjectId
+                , CatalogObjectType: null
+                , Quantity: changeQuantity.ToString()
+                , OccurredAt: DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ss.fffZ")
+                );
+
+            var itemChange = new InventoryChange(InventoryChange.TypeEnum.ADJUSTMENT
+                , PhysicalCount: null
+                , Adjustment: itemAdjustment
+                , Transfer: null);
 
             BatchChangeInventoryRequest req = new BatchChangeInventoryRequest();
-            req.IdempotencyKey = guid.ToString();
-            req.Changes = changes;
-            var response = inventoryAPI.BatchChangeInventory(req);
+            req.IdempotencyKey = Guid.NewGuid().ToString();
+            req.Changes = new List<InventoryChange>() { itemChange };
+
+            try
+            {
+                var response = inventoryAPI.BatchChangeInventoryWithHttpInfo(req);
+                if(response != null && response.StatusCode != (int)HttpStatusCode.OK)
+                {
+                    return new ApiResponse(response.Data.Errors, false);
+                }
+
+                return new ApiResponse(response.Data.Errors, true);
+            } catch(ApiException e)
+            {
+                return new ApiResponse(e.Message, false);
+            } catch(Exception e)
+            {
+                return new ApiResponse(e.Message, false);
+            }
         }
     }
 }
