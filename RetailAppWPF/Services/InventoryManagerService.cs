723adf7a69c27ecf2875f24605054bfd1a26d7a9
Zachary Bethem <zbethem@outlook.com>
2022-06-14 18:31:34 -0700

    connected inventory service to view model structure

---
+++ RetailAppWPF/Services/InventoryManagerService.cs
@@ -0,0 +1,31 @@
+﻿using RetailApp.Data;
+using RetailAppWPF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetailAppWPF.Services
+{
+    public class InventoryManagerService
+    {
+        private const string SUCCESS_RESULT = "Successful Inventory Update";
+
+        public (bool IsSuccess, string Message) AddInventory(ProductItem product, int addQuantity)
+        {
+            var svc = new InventoryService();
+            var result = svc.AddInventory(product.VariationId, addQuantity);
+
+            return ParseApiResponse(result);
+        }
+
+        private (bool IsSuccess, string Message) ParseApiResponse(ApiResponse response)
+        {
+            if (response.IsSuccess)
+                return (response.IsSuccess, SUCCESS_RESULT);
+            else
+                return (response.IsSuccess, response.Errors[0].Message);
+        }
+    }
+}
